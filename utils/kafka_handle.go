c7a74def8f40d48948578aaca945333b365aa734
Nicke-lucky <13965882371@163.com>
2020-07-28 22:27:43 +0800

    结算监控

--- utils/kafka_handle.go
+++ utils/kafka_handle.go
@@ -3,7 +3,9 @@ package utils
 import (
 	"bufio"
 	"context"
+	"flag"
 	"fmt"
+	log "github.com/sirupsen/logrus"
 	"os"
 
 	"github.com/segmentio/kafka-go"
@@ -13,7 +15,7 @@ const (
 	kafkaConn1 = "127.0.0.1:9092"
 	kafkaConn2 = "127.0.0.1:9093"
 	kafkaConn3 = "127.0.0.1:9094"
-	topic      = "test_kafka"
+	topic1     = "test_kafka"
 )
 
 //代理
@@ -23,7 +25,7 @@ var brokerAddrs = []string{kafkaConn1, kafkaConn2, kafkaConn3}
 func KafkaProduction() {
 	// read command line input
 	reader := bufio.NewReader(os.Stdin)
-	writer := newKafkaWriter(brokerAddrs, topic)
+	writer := newKafkaWriter(brokerAddrs, topic1)
 	defer writer.Close()
 	for {
 		fmt.Print("Enter msg: ")
@@ -45,4 +47,36 @@ func newKafkaWriter(kafkaURL []string, topic string) *kafka.Writer {
 		Brokers: kafkaURL,
 		Topic:   topic,
 	})
+}
+
+var (
+	topic2 = flag.String("t", "test_kafka", "kafka topic")
+	group  = flag.String("g", "test-group", "kafka consumer group")
+)
+
+func main() {
+	flag.Parse()
+	config := kafka.ReaderConfig{
+		Brokers:  []string{kafkaConn1, kafkaConn2, kafkaConn3},
+		Topic:    *topic2,
+		MinBytes: 1e3,
+		MaxBytes: 1e6,
+		GroupID:  *group,
+	}
+	reader := kafka.NewReader(config)
+	ctx := context.Background()
+	for {
+		msg, err := reader.FetchMessage(ctx)
+		if err != nil {
+
+			log.Printf("fail to get msg:%v", err)
+			continue
+		}
+		log.Printf("msg content:topic=%v,partition=%v,offset=%v,content=%v",
+			msg.Topic, msg.Partition, msg.Offset, string(msg.Value))
+		err = reader.CommitMessages(ctx, msg)
+		if err != nil {
+			log.Printf("fail to commit msg:%v", err)
+		}
+	}
 }
